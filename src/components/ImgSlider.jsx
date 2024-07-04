8b30bf6e63965c28f012ba5eabc7e5b1b83d538a
Prashant Kumar Dewangan <dewanganprashant47@gmail.com>
2024-07-04 17:30:38 +0530

    Updated ImgSlider component with new functionality and styles, and made minor adjustments to DetailPage styles.

--- src/components/ImgSlider.jsx
+++ src/components/ImgSlider.jsx
@@ -1,13 +1,71 @@
 import React from "react";
-import style from "./ImgSlider.module.css";
+import { useState } from "react";
+
+import Style from "./ImgSlider.module.css";
 
 function ImgSlider({ images }) {
+  //states of a component
+  const [imageNum, setImageNumber] = useState(null);
+
+  // variable
+  let totalImg = images.length;
+
+  //handler function
+  function handleImgInc() {
+    setImageNumber((num) => (num + 1) % totalImg);
+  }
+
+  function handleImgDec() {
+    setImageNumber((num) => (num - 1 + totalImg) % totalImg);
+  }
   return (
-    <div className={style.ImgContainer}>
-      {/* <div className={style.largeImg}>
-        <img src={`${images[0]}`} />
-      </div> */}
-      slider
+    <div className={Style.imgContainer}>
+      {imageNum !== null && (
+        <div className={Style.fullSlider}>
+          <div className={Style.arrow}>
+            <img
+              src="/arrow.png"
+              alt=""
+              style={{ width: "48px" }}
+              onClick={handleImgDec}
+            />
+          </div>
+          <div className={Style.imgSlider}>
+            <img src={`${images[imageNum]}`} alt="" />
+          </div>
+          <div className={Style.arrow}>
+            <img
+              src="/arrow.png"
+              alt=""
+              onClick={handleImgInc}
+              style={{ width: "48px" }}
+            />
+          </div>
+        </div>
+      )}
+      <div className={Style.bigImg}>
+        <img
+          src={`${images[0]}`}
+          alt="big images"
+          onClick={() => setImageNumber(0)}
+        />
+      </div>
+      <div className={Style.close_btn} onClick={() => setImageNumber(null)}>
+        X
+      </div>
+      <div className={Style.smallImg}>
+        {images.map((image, index) => {
+          if (index === 0) return null;
+          return (
+            <img
+              key={index}
+              src={`${image}`}
+              alt="small images"
+              onClick={() => setImageNumber(index)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }

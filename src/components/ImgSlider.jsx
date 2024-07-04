6ab7d5a56bef888b7e7c81a2379a0d3134f47285
Prashant Kumar Dewangan <dewanganprashant47@gmail.com>
2024-07-04 15:21:19 +0530

    Updated CSS styles for DetailPage, added ImgSlider component, and modified temp.js data structure and DetailPage.jsx to display property details and user information.

---
+++ src/components/ImgSlider.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./ImgSlider.module.css";
+
+function ImgSlider({ images }) {
+  return (
+    <div className={style.ImgContainer}>
+      {/* <div className={style.largeImg}>
+        <img src={`${images[0]}`} />
+      </div> */}
+      slider
+    </div>
+  );
+}
+
+export default ImgSlider;
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -2,12 +2,45 @@ import React from "react";
 import style from "./DetailPage.module.css";
 import { useParams } from "react-router-dom";
 
+import ImgSlider from "../components/ImgSlider";
+import { singlePostData, userData } from "../lib/temp";
+
 function DetailPage() {
   const { houseId } = useParams();
   return (
     <div className={style.detailPage}>
-      <div className={style.details}></div>
-      <div className={style.fetures}></div>
+      <div className={style.details}>
+        <div className={style.wrapper}>
+          <div className={style.top}>
+            <ImgSlider images={singlePostData.images} />
+            <div className={style.info}>
+              <div className={style.desc}>
+                <h1>{singlePostData.title}</h1>
+                <div className={style.address}>
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <p className={style.price}>
+                  <span>$</span>
+                  {singlePostData.price}
+                </p>
+              </div>
+              <div className={style.user}>
+                <img
+                  className={style.userImg}
+                  src={`${userData.img}`}
+                  alt="avatar"
+                />
+                <p>{userData.name}</p>
+              </div>
+            </div>
+          </div>
+          <div className={style.bottom}>{singlePostData.description}</div>
+        </div>
+      </div>
+      <div className={style.fetures}>
+        <div className={style.wrapper}></div>
+      </div>
     </div>
   );
 }

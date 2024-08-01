d01fd43c1b0db94c9e22c55adcb4d2e14edb9f93
Prashant Kumar Dewangan <135017151+Prashant-12-08@users.noreply.github.com>
2024-08-01 16:16:39 +0530

    Update vite.config.js

--- realEstateApp/vite.config.js
+++ realEstateApp/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      // '/api': 'http://127.0.0.1:3000',
-      '/api': 'https://realestateapi-rbhr.onrender.com/',
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     // '/api': 'http://127.0.0.1:3000',
+  //     '/api': 'https://realestateapi-rbhr.onrender.com/',
+  //   },
+  // },
   plugins: [react()],
 });
